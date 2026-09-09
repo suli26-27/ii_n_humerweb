002dd26ee592cd6327867f7f6e4891dfd420aa9a
Gyenge Elemér <gyenge@valahol.lan>
2026-09-09 09:26:40 +0200

    Navigáció kész

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PositionComponent } from './position/position.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'employee', component: EmployeeComponent},
+    { path: 'position', component: PositionComponent}
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-home',
+  styleUrl: './home.component.css',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {}
